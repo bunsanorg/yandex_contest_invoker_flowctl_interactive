0c8d34a937e5d7c4eae31c02f42b164dffec42cf
Aleksey Filippov <sarum9in@gmail.com>
2014-04-02 05:42:04 +0400

    Error declaration.

---
+++ include/yandex/contest/invoker/flowctl/interactive/Error.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <yandex/contest/invoker/Error.hpp>
+
+namespace yandex{namespace contest{namespace invoker{
+    namespace flowctl{namespace interactive
+{
+    struct Error: virtual invoker::Error {};
+}}}}}
